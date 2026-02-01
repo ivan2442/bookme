808d111d55d059fdbb969e87dbaf635c90b18877
ivan2442 <ivanjasensky@gmail.com>
2026-02-01 05:12:39 +0100

    Zoom disable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="sk">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>{{ $title ?? 'BookMe' }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Rýchle online rezervácie pre služby na jednom mieste.">
